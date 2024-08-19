1bb414ccf771a0f90b835e608cd05f411502d493
cexithor <warchocobo@gmail.com>
2024-08-19 00:03:18 -0600

    Added landing invitation Dialog

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,75 @@
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
+import { Typography, Dialog, Box, Container, Button } from "@mui/material";
+import coupleNames from "./assets/couple-names.svg";
+import lineArtDivider01 from "./assets/line-art-divider-01.svg";
+import lineArtDivider02 from "./assets/line-art-divider-02.svg";
 
-function App() {
-  return <div>Hola</div>;
-}
+const dialogStyle = {
+  "& .MuiPaper-root": {
+    backgroundColor: "#283618",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    overflowX: "hidden",
+  },
+};
+
+const topArtStyle = {
+  position: "relative",
+  right: "50%",
+};
+
+const caption = {
+  color: "white",
+  fontSize: "12px",
+  fontStyle: "italic",
+  alignSelf: "flex-start",
+  padding: "0 16px",
+  margin: "16px 0 32px",
+};
+
+const coupleStyle = {
+  padding: "0 16px",
+};
+
+const bottomArtStyle = {
+  position: "relative",
+  left: "50%",
+};
+
+const buttonStyle = {
+  alignSelf: "flex-end",
+  margin: "32px 16px 24px",
+};
+
+const App = () => {
+  return (
+    <Box height="100vh" bgcolor="#fefae0">
+      <Dialog sx={dialogStyle} open={true} fullScreen>
+        {/* {transition((style) => (
+          <Favorite sx={{ color: "white", ...style }} />
+        ))} */}
+        <img
+          src={lineArtDivider01}
+          alt="line-art-divider"
+          style={topArtStyle}
+        />
+        <Typography sx={caption}>
+          Te invitamos cordialmente <br />a nuestra boda:
+        </Typography>
+        <img src={coupleNames} alt="couple-names" style={coupleStyle} />
+        <Button variant="contained" sx={buttonStyle}>
+          Abrir
+        </Button>
+        <img
+          src={lineArtDivider02}
+          alt="line-art-divider"
+          style={bottomArtStyle}
+        />
+      </Dialog>
+      <Container maxWidth="lg"></Container>
+    </Box>
+  );
+};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+// Fonts
+import "@fontsource-variable/montserrat";
+//
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "'Montserrat Variable', sans-serif",
+    fontWeightRegular: 300,
+  },
+  palette: {
+    primary: {
+      main: "#606c38",
+    },
+    secondary: {
+      main: "#dda15e",
+    },
+  },
+  components: {
+    MuiButton: {
+      style: {
+        borderRadius: "0px",
+        textTransform: "none",
+        fontWeight: 300,
+      },
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <CssBaseline />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
