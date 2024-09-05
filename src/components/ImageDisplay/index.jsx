6a3aff712addaf77a54b1ebea94435ae1080b0f0
cexithor <warchocobo@gmail.com>
2024-09-04 20:37:11 -0600

    Added pictures, changed colors

--- src/components/ImageDisplay/index.jsx
+++ src/components/ImageDisplay/index.jsx
@@ -1,9 +1,9 @@
 import { Box } from "@mui/material";
 import styles from "./styles";
 
-const ImageDisplay = ({ img }) => {
-  const backgroundImage = `url('${img}')`;
-  return <Box sx={{ ...styles.imgContainer, backgroundImage }} />;
+const ImageDisplay = (props) => {
+  const backgroundImage = `url('${props.img}')`;
+  return <Box sx={{ ...styles.imgContainer, backgroundImage }} {...props} />;
 };
 
 export default ImageDisplay;
--- src/utils/itemData.js
+++ src/utils/itemData.js
@@ -1,51 +1,51 @@
 const itemData = [
   {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-    title: 'Breakfast',
+    img: 'images/couple-picture-12.jpg',
+    title: 'Vale y Josue',
   },
   {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Burger',
+    img: 'images/couple-picture-11.jpg',
+    title: 'Vale y Josue',
   },
   {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
+    img: 'images/couple-picture-10.jpg',
+    title: 'Vale y Josue',
   },
   {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
+    img: 'images/couple-picture-8.jpg',
+    title: 'Vale y Josue',
   },
   {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Hats',
+    img: 'images/couple-picture-7.jpg',
+    title: 'Vale y Josue',
   },
   {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Honey',
+    img: 'images/couple-picture-6.jpg',
+    title: 'Vale y Josue',
   },
   {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-    title: 'Basketball',
+    img: 'images/couple-picture-5.jpg',
+    title: 'Vale y Josue',
   },
   {
-    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-    title: 'Fern',
+    img: 'images/couple-picture-4.jpg',
+    title: 'Vale y Josue',
   },
   {
-    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-    title: 'Mushrooms',
+    img: 'images/couple-picture-3.jpg',
+    title: 'Vale y Josue',
   },
   {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
+    img: 'images/couple-picture-2.jpg',
+    title: 'Vale y Josue',
   },
   {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
+    img: 'images/couple-picture-1.jpg',
+    title: 'Vale y Josue',
   },
   {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
+    img: 'images/couple-picture-0.jpg',
+    title: 'Vale y Josue',
   },
 ];
 
